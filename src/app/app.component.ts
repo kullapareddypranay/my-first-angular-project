74be519e8e01ebde26926d5b0c20cf6dd1b7a184
pranay reddy <kpranayreddy2000@gmail.com>
2020-06-11 11:26:21 +0530

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'components';
+  title:string = 'components';
+  recipeName:string='';
+  recipes=[];
+
+  onAdd(){
+    this.recipes.push(this.recipeName)
+  }
+
+  onRemove(){
+   var i=0;
+   while(i<this.recipes.length){
+     if(this.recipes[i]===this.recipeName){
+       this.recipes.splice(i,1);
+     }else{
+       ++i;
+     }
+   }
+  }
 }
