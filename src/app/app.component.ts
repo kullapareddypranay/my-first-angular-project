2a93227c7891bb934b4ff88882be3b9e8a140707
pranay reddy <kpranayreddy2000@gmail.com>
2020-06-11 16:13:41 +0530

    added clear, remove all and remove option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:string = 'components';
+  title:string = 'List';
   recipeName:string='';
   recipes=[];
 
   onAdd(){
-    this.recipes.push(this.recipeName)
+    this.recipes.push(this.recipeName);
+    this.recipeName='';
   }
 
-  onRemove(){
-   var i=0;
-   while(i<this.recipes.length){
-     if(this.recipes[i]===this.recipeName){
-       this.recipes.splice(i,1);
-     }else{
-       ++i;
-     }
+  onRemove(name){
+   var index=this.recipes.indexOf(name);
+   if(index>-1){
+     this.recipes.splice(index,1);
    }
+  }
+
+
+  onRemoveAll(){
+    // var i:number=0;
+    // while(i<this.recipes.length){
+    //   if(this.recipes[i]===this.recipeName){
+    //     this.recipes.splice(i,1);
+    //   }else{
+    //     ++i;
+    //   }
+    // }
+
+    this.recipes=[];
+  
+  }
+
+
+  clear(){
+    this.recipeName='';
   }
 }
